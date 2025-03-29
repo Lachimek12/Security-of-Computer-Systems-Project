9d7faf037c765748a540e359b3225f7e5c130268
Julian Kulikowski <julkul@onet.pl>
2025-03-29 15:25:41 +0100

    Add main app base foundation

---
+++ Main/MainApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MainApp.ViewModels;
+
+namespace MainApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Main_Clicked(null, null);
+        }
+
+        private void Main_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Width = 1210;
+            this.Height = 830;
+            this.ResizeMode = ResizeMode.CanResize;
+            MainNavbarButton.Visibility = Visibility.Collapsed;
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ Main/MainApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MainApp.Commands;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.Intrinsics.X86;
+using System.Printing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+using System.Windows.Media.Media3D;
+
+namespace MainApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Consturctors
+        public MainViewModel()
+        {
+
+            // this.ExampleCommand = new RelayCommand(() => {});
+
+
+        }
+
+        #endregion
+
+
+        #region Members
+
+
+        #endregion
+
+
+        #region Properties
+
+        /*
+        public string Example
+        {
+            get { return this._example; }
+            set
+            {
+                if (value != this._example)
+                {
+                    this._example = value;
+                    RaisePropertyChanged(nameof(Example));
+                }
+            }
+        }
+        */
+
+
+
+        // public ICommand ExampleCommand { get; }
+
+
+        #endregion
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+    }
+
+}
